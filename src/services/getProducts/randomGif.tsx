1f4df15844de5a0a930e9e0c0d26352ec61470b2
DESKTOP-1J9IC8J\Daniel <d.ghaemi.airtour@gmail.com>
2024-02-12 00:56:48 +0330

    add loading to product list page, handle search logic, handle product list api calling a getting the response

--- src/services/getProducts/randomGif.tsx
+++
@@ -1,7 +0,0 @@
-import {request} from '../../network';
-import {getProductList} from '../constants/urlConstants';
-
-export const getProductListDetail = async () => {
-  const response = await request.axiosGet(getProductList());
-  return response;
-};
